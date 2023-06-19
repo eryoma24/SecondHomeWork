61375909423b709e278c105db3ab36f478f4af4b
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-19 19:39:34 +0600

    added file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это не день недели!");
+    return false;
+}
+
+int weekDay = Prompt("Введите цифру дня недели => ");
+if (ValidateWeekday(weekDay))
+{
+    if(IsWeekend(weekDay))
+    {
+        System.Console.WriteLine("Наконец-то выходной");
+    }
+    else
+    {
+        System.Console.WriteLine("Придётся поработать");
+    }
+}
